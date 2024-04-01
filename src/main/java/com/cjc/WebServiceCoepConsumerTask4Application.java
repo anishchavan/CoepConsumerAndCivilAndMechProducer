5a651d06dd935cab62e0102b0c71d154828a553f
anishchavan <anishchavan29@gmail.com>
2024-04-01 23:26:21 +0530

    initial commit

---
+++ src/main/java/com/cjc/WebServiceCoepConsumerTask4Application.java
@@ -0,0 +1,20 @@
+package com.cjc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class WebServiceCoepConsumerTask4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceCoepConsumerTask4Application.class, args);
+	}
+
+	@Bean
+	public RestTemplate rt() {
+		RestTemplate rs = new RestTemplate();
+		return rs;
+	}
+}
